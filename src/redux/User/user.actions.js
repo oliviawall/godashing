08b3fdfdf9bb270a44e5af9e7e48e1971dba9c66
Olivia Katelyn <theoliviakatelyn@Olivias-MacBook-Pro.local>
2020-10-06 20:02:38 -0600

    more user sagas

--- src/redux/User/user.actions.js
+++ src/redux/User/user.actions.js
@@ -1,6 +1,37 @@
 import userTypes from './user.types';
 import { auth, handleUserProfile, GoogleProvider } from './../../firebase/utils';
 
+
+export const emailSignInStart = userCredentials => ({
+    type: userTypes.EMAIL_SIGN_IN_START,
+    payload: userCredentials
+});
+
+export const signInSuccess = user => ({
+    type: userTypes.SIGN_IN_SUCCESS,
+    payload: user
+});
+
+export const checkUserSession = user => ({
+    type: userTypes.CHECK_USER_SESSION,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const setCurrentUser = user => ({
     type: userTypes.SET_CURRENT_USER,
     payload: user
@@ -10,18 +41,9 @@ export const resetAllAuthForms = () => ({
     type: userTypes.RESET_AUTH_FORMS
 });
 
-export const signInUser = ({ email, password }) => async dispatch => {
-    try {
-        await auth.signInWithEmailAndPassword(email, password);
-        dispatch({
-            type: userTypes.SIGN_IN_SUCCESS,
-            payload: true
-        });
-
-    } catch (err) {
-        // console.log(err);
-    }
-};
+// export const signInUser = ({ email, password }) => async dispatch => {
+//     
+// };
 
 export const signUpUser = ({ displayName, email, password, confirmPassword }) => async dispatch => {
     if (password !== confirmPassword) {
