3d8700d172611f9705fc6da5e9c0aa6f6a8b61ee
Matthew Gunderson <matthew.gunderson12@gmail.com>
2020-12-27 17:27:24 -0800

    stash, wip

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -24,7 +24,7 @@ export default function CarouselHome() {
                                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
-                        <Carousel.Item interval={500}>
+                        <Carousel.Item interval={3000}>
                             <img
                                 className='d-block w-100'
                                 src={RainImg}
@@ -35,7 +35,7 @@ export default function CarouselHome() {
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
-                        <Carousel.Item>
+                        <Carousel.Item interval={3000}>
                             <img
                                 className='d-block w-100'
                                 src={WalkingSlide}
